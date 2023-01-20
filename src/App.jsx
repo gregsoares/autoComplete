d0bf8236f439664308e08594f0cd2af340c254c4
gsoares <gsoares@wsgc.com>
2023-01-20 12:08:41 -0500

    added json files with all data needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+/** TODO:
+ *
+ * -- Call the API to get the data (from json files)
+ * -- Load all the data into the state
+ * -- Pass the data to the components
+ *
+ *
+ */
 function App() {
   return <div className='bg-slate-300 min-h-screen '></div>
 }
