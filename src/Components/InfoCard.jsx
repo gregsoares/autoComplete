77de41360a051510e1c85d4557e8eb1d5edc87d4
Greg Soares <gregselvagem@gmail.com>
2023-02-19 11:57:10 -0500

    quick infoCard refactor

--- src/Components/InfoCard.jsx
+++ src/Components/InfoCard.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 //@TODO: make displayData function returning styled JSX
 
 const InfoCard = ({ data }) => {
+  function DisplayResults() {
+    return Object.entries(data[0]).map((item, index) => (
+      <p className='px-2 py-1' key={item[0] + index}>
+        <b className='capitalize'>{item[0]}</b>: {item[1]}
+      </p>
+    ))
+  }
   return (
     <div className='flex justify-center'>
       InfoCard:
       <section className='flex shadow-md rounded-md bg-slate-500 mx-2 my-1 max-w-lg'>
         <div className='flex flex-col items-center'>
-          <div className='m-1'>
-            {Object.entries(data[0]).map((item, index) => (
-              <p className='px-2 py-1' key={item[0] + index}>
-                <b className='capitalize'>{item[0]}</b>: {item[1]}
-              </p>
-            ))}
-          </div>
+          <div className='m-1'>{DisplayResults()}</div>
         </div>
       </section>
     </div>
